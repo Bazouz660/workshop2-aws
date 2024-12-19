a23d6f4e8a2f1bbb1733249f1bfeb88075323d50
Bazouz660 <basile.trebus-hamann@epitech.eu>
2024-12-19 20:41:19 +0100

    First commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!--NuxtRouteAnnouncer />
+    <NuxtWelcome /-->
+    
+
+    <form  @submit="submit">
+      Name and lastname
+      <input type="text" name="name"/><br>
+      Email
+      <input type="email" name="name"/><br>
+
+      <button type="submit" >Save</button>
+    </form>
+    
+  </div>
+</template>
+
+
+<script setup>
+
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
+</script>
